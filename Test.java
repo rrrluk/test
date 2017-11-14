a11ac4732c424f5135ea123e9ac8494b0eea8b94
Pertti Johanson <rrrluk@gmail.com>
2017-11-14 16:52:32 +0200

    Plap change

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ public class Test{
     public static void main(String[] args) {
         for(int i=1;i<=10;i++)
         {
-            System.out.println("Git, World");
+            System.out.println("Plap!");
         }
     }
 }
